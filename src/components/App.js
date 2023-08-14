7bc8568af35ead194cbf1d6b200c73ccd78d6e72
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-14 22:12:11 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Child from './Child'
 
 const App = () => {
+  let [cartItems,SetcartItems]= useState([{name:'Item 1', price:'10'},{name:'Item 2', price:'20'},{name:'Item 3', price:'35'},{name:'Item 4', price:'40'}])
+  function additem(){
+    let c={}
+    c.name=document.getElementById('itemName').value
+    c.price=document.getElementById('itemPrice').value
+    console.log('1')
+    document.getElementById('itemName').value=''
+    document.getElementById('itemPrice').value=''
+    SetcartItems([...cartItems,c])
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className='parent'>
+      <h1>Parent Component</h1>
+      <label>Item Name:</label>
+      <input type='text' id='itemName'></input>
+      <label>Item Price:</label>
+      <input type='number' id='itemPrice'></input>
+      <button onClick={additem}>Add Item</button>
+        {<Child  set={SetcartItems} items={cartItems}/>}
     </div>
   )
 }
---
+++ src/components/Child.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+let Child= (props)=>{
+    function changedata(name){
+        for(let i=0;i<props.items.length;i++){
+            if(props.items[i].name==name){
+                let d=[...props.items]
+                d.splice(i,1)
+                props.set(d)
+                break
+            }
+        }
+    }
+return (
+    <div className='child'>
+    <h1>Child Component</h1>
+    {props.items.map(element=>(
+        <l1>
+            {element.name}-{'$'+element.price}
+            <button onClick={()=>changedata(element.name)}>Remove</button>
+        </l1>
+       
+    ))}
+    </div>
+)
+}
+export default Child
